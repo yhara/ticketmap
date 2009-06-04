b856377ea7b439676c611cd62b1564fb485eaf60
Yutaka HARA <yutaka.hara+github@gmail.com>
2009-06-04 13:36:12 +0900

    Added test of renaming tickets

--- spec/tickets.rb
+++ spec/tickets.rb
@@ -4,6 +4,10 @@ load 'bin/tickets-server'
 describe TicketsController do
   behaves_like :rack_test
 
+  before do
+    Ticket.delete
+  end
+
   # create
 
   it 'creates new ticket' do
@@ -11,4 +15,22 @@ describe TicketsController do
       get('/tickets/create')
     }.should.change{ Ticket.count }
   end
+
+  # rename
+
+  it 'renames a ticket' do
+    get('/tickets/create').body =~ /\A\(id . (\d+)\)\z/
+    id = $1
+    id.should.not == nil
+
+    post('/tickets/rename', :id => id, :title => "test1")
+    last_response.body.should == "#t"
+    Ticket.find(:id => id).title.should == "test1"
+  end
+
+  # list
+
+#  it 'enumerates created tickets' do
+#    get('/tickets/list').should =~ 
+#  end
 end
