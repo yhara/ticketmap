5e664bc051b637f37f022cc5edd3dc0bfeb52a70
Yutaka HARA <yutaka.hara+github@gmail.com>
2009-06-04 13:46:32 +0900

    Added test of remaining apis

--- spec/tickets.rb
+++ spec/tickets.rb
@@ -12,25 +12,54 @@ describe TicketsController do
 
   it 'creates new ticket' do
     lambda{
-      get('/tickets/create')
+      get('/tickets/create').should =~ /\A\(id . (\d+)\)\z/
     }.should.change{ Ticket.count }
+  end
+  
+  # list
+
+  it 'enumerates created tickets' do
+    Ticket.create(:title => "test1")
+    Ticket.create(:title => "test2")
+
+    get('/tickets/list')
+
+    last_response.should =~ /test1/
+    last_response.should =~ /test2/
+  end
+  
+  # list
+
+  it 'moves a ticket' do
+    id = get('/tickets/create').match(/\A\(id . (\d+)\)\z/)[1]
+
+    post('/tickets/move', :id => id, :x => 10, :y => -10)
+    last_response.body.should == "#t"
+
+    Ticket.first.emergency.should == 10
+    Ticket.first.importance.should == -10
   end
 
   # rename
 
   it 'renames a ticket' do
-    get('/tickets/create').body =~ /\A\(id . (\d+)\)\z/
-    id = $1
-    id.should.not == nil
+    id = get('/tickets/create').match(/\A\(id . (\d+)\)\z/)[1]
 
     post('/tickets/rename', :id => id, :title => "test1")
     last_response.body.should == "#t"
-    Ticket.find(:id => id).title.should == "test1"
+
+    Ticket.first.title.should == "test1"
   end
 
-  # list
+  # delete
+  
+  it 'deletes a ticket' do
+    id = get('/tickets/create').match(/\A\(id . (\d+)\)\z/)[1]
 
-#  it 'enumerates created tickets' do
-#    get('/tickets/list').should =~ 
-#  end
+    post('/tickets/delete', :id => id)
+    last_response.body.should == "#t"
+
+    Ticket.first.deleted.should == true
+  end
+
 end
