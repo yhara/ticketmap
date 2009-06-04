19e3efaf56dc2b6a53eb56f5b85f8350d5f97bc2
Yutaka HARA <yutaka.hara+github@gmail.com>
2009-06-04 22:42:43 +0900

    Use Innate::Optioned

--- model/ticket.rb
+++ model/ticket.rb
@@ -2,26 +2,26 @@ require 'sequel'
 require 'sequel/extensions/migration'
 Sequel::Model.plugin(:schema) # for table_exists?
 
-$db = Sequel.sqlite(Tickets::Config::DB_PATH)
+$db = Sequel.sqlite(TicketMap.options[:db])
 
 class Ticket < Sequel::Model(:tickets)
   @@last_shook = Time.now
 
   def self.needs_shaking?
-    (Time.now - @@last_shook) > Tickets::Config::SHAKE_INTERVAL*60*60
+    (Time.now - @@last_shook) > TicketMap.options[:shake_interval]*60*60
   end
 
   def self.shake!
     Ticket.each do |ticket|
       pos = ticket.emergency
       dir = (ticket.importance < 0 ? 1 : -1)
-      newpos = pos + Tickets::Config::SHAKE_DISTANCE * dir
-      if newpos > (Tickets::Config::BOARD_WIDTH / 2)
-        newpos = (Tickets::Config::BOARD_WIDTH / 2)
+      newpos = pos + TicketMap.options[:shake_distance] * dir
+      if newpos > (TicketMap.options[:board_width] / 2)
+        newpos = (TicketMap.options[:board_width] / 2)
       end
 
       ticket.update(:emergency => newpos) if newpos != pos
-      if newpos < -(Tickets::Config::BOARD_WIDTH / 2)
+      if newpos < -(TicketMap.options[:board_width] / 2)
         ticket.update(:deleted => true, :timeouted => true)
       end
     end
